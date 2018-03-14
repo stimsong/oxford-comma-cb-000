165bc6e3fd959324e1a9e3f5732f8b97544ef5e5
Greg Stimson <stimsong@yahoo.com>
2018-03-14 00:17:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   joined_array = []
 
-  if array.count = 1
+  if array.length = 1
     joined_array = array.to_s
-  elsif array.count = 2
+  elsif array.length = 2
     joined_array = array.join(" and ")
   else
     joined_array = array.join(", ")
