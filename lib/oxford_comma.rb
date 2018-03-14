6dd7113e23add4b12e4e08b7696f891ba7f56f39
Greg Stimson <stimsong@yahoo.com>
2018-03-14 00:33:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,8 @@ def oxford_comma(array)
 
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
-#  elsif count = 2
-
-  else
-    array.join(", ")
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
   end
-#  joined_array
+  array.join(", ")
 end
